f788e9fa9b43ffdb092c1155d6a261f522c9cbb3
Cristian <cristian.morenos@outlook.com>
2020-12-29 16:45:35 -0300

    Funcion consultar canciones

---
+++ consultar.js
@@ -0,0 +1,25 @@
+const {
+    Pool
+} = require("pg")
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    password: "postgre",
+    port: 5432,
+    database: "repertorio"
+})
+
+const consultar = async () => {
+    const consulta = "SELECT * FROM repertorio"
+    try {
+        const res = await pool.query(consulta)
+        return res.rows
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    consultar
+}
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const url = require("url");
 const http = require("http");
 const fs = require("fs");
+const {
+    consultar
+} = require("./consultar")
 
 
-http.createServer((req, res) => {
-    
+http.createServer(async (req, res) => {
+    const {
+        id
+    } = url.parse(req.url, true).query;
 
-    
-
+    if (req.url == "/" && req.method == "GET") {
+        res.setHeader("content-type", "text/html")
+        const html = fs.readFileSync("index.html", "utf8")
+        res.end(html)
+    }
+    if (req.url == "/canciones" && req.method == "GET") {
+        const registros = await consultar()
+        res.end(JSON.stringify(registros))
+    }
 
 }).listen(3000);
