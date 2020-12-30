347915436c43362b5868e7045fab76232e2ec8e5
Brian Habib <brianhft20@gmail.com>
2020-12-30 10:30:51 -0300

    added ruta

--- editar.js
+++ editar.js
@@ -1,28 +1,27 @@
-
 const editar = async (datos, pool) => {
-    const consulta = {
-      text: `UPDATE repertorio SET cancion = $1, artista = $2, tono = $3 WHERE cancion =$1 RETURNING *`,
-      values: datos,
-    };
-    try {
-      const result = await pool.query(consulta);
-      return result;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
+  const consulta = {
+    text: `UPDATE repertorio SET cancion = $1, artista = $2, tono = $3 WHERE cancion =$1 RETURNING *`,
+    values: datos,
   };
-
-  module.exports = { editar };
-
-  if (req.url == "/cancion" && req.method == "PUT") {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", async () => {
-      const datos = Object.values(JSON.parse(body));
-      const respuesta = await editar(datos, pool);
-      res.end(JSON.stringify(respuesta));
-    });
+  try {
+    const result = await pool.query(consulta);
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
+};
+
+module.exports = { editar };
+
+if (req.url == "/cancion" && req.method == "PUT") {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+  req.on("end", async () => {
+    const datos = Object.values(JSON.parse(body));
+    const respuesta = await editar(datos, pool);
+    res.end(JSON.stringify(respuesta));
+  });
+}
